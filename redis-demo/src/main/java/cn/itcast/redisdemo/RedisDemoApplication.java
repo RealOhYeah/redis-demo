1cf1c9d77998ef0870f14f31cbbf6d1a22c6f07e
Oh...Yeah!!! <614988210@qq.com>
2024-04-29 15:58:04 +0800

    配置读写分离

--- redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
+++ redis-demo/src/main/java/cn/itcast/redisdemo/RedisDemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 
 @SpringBootApplication
 public class RedisDemoApplication {
@@ -12,5 +13,23 @@ public class RedisDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(RedisDemoApplication.class, args);
     }
+
+    // 替换成lambda
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return clientConfigurationBuilder -> clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+    }
+
+    /* 匿名内部类版本
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer clientConfigurationBuilderCustomizer(){
+        return new LettuceClientConfigurationBuilderCustomizer() {
+            @Override
+            public void customize(LettuceClientConfiguration.LettuceClientConfigurationBuilder clientConfigurationBuilder) {
+                clientConfigurationBuilder.readFrom(ReadFrom.REPLICA_PREFERRED);
+            }
+        };
+    }
+    */
 
 }
